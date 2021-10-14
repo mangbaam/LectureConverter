58fb147c976a3132ba252c264d703286f5df41ed
mangbaam <pmb0836@gmail.com>
2021-10-14 22:30:47 +0900

    realtimeDB에 강의 업데이트

--- main.py
+++ main.py
@@ -1,7 +1,67 @@
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import json
 
-# Press the green button in the gutter to run the script.
+path = "C:/Users/audqj/OneDrive - suwon.ac.kr/자료/classmate/강의목록/realtimeDB.json"
+lecturePath = "C:/Users/audqj/OneDrive - suwon.ac.kr/자료/classmate/강의목록/USW_2021_2.json"
+translate = {
+    "subjtCd": "과목 코드",
+    "subjtNm": "과목명",
+    "timtSmryCn": "시간표",
+    "ltrPrfsNm": "교수자",
+    "estbDpmjNm": "개설 부서",
+    "point": "학점",
+    "facDvnm": "이수구분",
+    "trgtGrdeCd": "개설 학년",
+    "cltTerrCd": "교양 영역",
+}
+cltTerr = {
+    "41": "1영역-언어와 소통",
+    "42": "2영역-세계와 문명",
+    "43": "3영역-역사와 사회",
+    "44": "4영역-문화와 철학",
+    "45": "5영역-기술과 정보",
+    "46": "6영역-건강과 예술",
+    "47": "7영역-자연과 과학"
+}
+
+
+def addLecture(school, term):
+    f = open(lecturePath, encoding='utf-8')
+    lectures = json.load(f)
+    f.close()
+    lectureList = dict()
+    for lecture in lectures:
+        # 폐강 과목 제외
+        if "closeDt" in lecture.keys() or "closeResnCn" in lecture.keys():
+            continue
+        # 키: 과목코드-분반
+        key = lecture["subjtCd"] + '-' + lecture["diclNo"]
+        # 과목 정보 저장
+        infos = dict()
+        for column in lecture.keys():
+            if column in translate.keys():
+                # 교양 영역 처리
+                if column == "cltTerrCd":
+                    infos[translate[column]] = cltTerr[lecture[column]]
+                else:
+                    infos[translate[column]] = lecture[column]
+        lectureList[key] = infos
+
+    f = open(path, encoding='utf-8')
+    realtimeDB = json.load(f)
+    f.close()
+
+    if school not in realtimeDB.keys():
+        raise Exception("사용 불가한 학교입니다.")
+    elif term not in realtimeDB[school].keys():
+        raise Exception("해당 학기 정보가 없습니다.")
+    else:
+        realtimeDB[school][term] = lectureList
+
+    # 기존의 DB 대체
+    outfile = open(path, 'w', encoding='utf-8')
+    json.dump(realtimeDB, outfile)
+    outfile.close()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    addLecture("USW", "2021_2")
